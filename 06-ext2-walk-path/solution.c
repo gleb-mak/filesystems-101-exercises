e305aa303e55b21f287cce27db71a742b1468b65
gleb-mak <makarov.ge@phystech.edu>
2022-11-13 22:57:49 +0300

    fixed solution

--- 06-ext2-walk-path/solution.c
+++ 06-ext2-walk-path/solution.c
@@ -62,21 +62,21 @@ int WriteFile(int img, int inode_nr, int out)
 {
 	struct ext2_super_block superblock;
 	if (pread(img, &superblock, sizeof(struct ext2_super_block), 1024) == -1)
-		return -errno;
+		return -1;
 	unsigned int inodes_per_group = superblock.s_inodes_per_group;
 	unsigned int block_group_num = (inode_nr - 1) / inodes_per_group;
 	unsigned int block_size = 1024 << superblock.s_log_block_size;
 	struct ext2_group_desc group_desc;
 	off_t group_offset = 1024 + block_size * superblock.s_blocks_per_group * block_group_num + block_size; //1024 in begining, block_size * superblock.s_blocks_per_group - size of block_group, block_size - size of superblock in needed group block
 	if (pread(img, &group_desc, sizeof(struct ext2_group_desc), group_offset) == -1)
-		return -errno;
+		return -1;
 	
 	unsigned int inode_index = (inode_nr - 1) % inodes_per_group;
 	unsigned int inode_size = superblock.s_inode_size;
 	struct ext2_inode inode;
 	off_t inode_offset = group_desc.bg_inode_table * block_size + inode_index * inode_size;
 	if (pread(img, &inode, inode_size, inode_offset) == -1) 
-		return -errno;
+		return -1;
 	unsigned int file_size = inode.i_size;
 	for (unsigned int block_count = 0; block_count < EXT2_N_BLOCKS; ++block_count) {
 		if (inode.i_block[block_count] == 0)
@@ -84,15 +84,15 @@ int WriteFile(int img, int inode_nr, int out)
 		off_t offset = inode.i_block[block_count] * block_size;
 		if (block_count < EXT2_NDIR_BLOCKS) {
 			if (WriteDataFromBlock(img, block_size, offset, &file_size, out) != 0)
-				return -errno;
+				return -1;
 		}
 		else if (block_count == EXT2_IND_BLOCK) {
 			if (WriteIndirectBlock(img, block_size, offset, &file_size, out) != 0)
-				return -errno;
+				return -1;
 		}
 		else if (block_count == EXT2_DIND_BLOCK) {
 			if (WriteDoubleBlock(img, block_size, offset, &file_size, out) != 0)
-				return -errno;
+				return -1;
 		}
 	}
 	/* implement me */
@@ -108,7 +108,7 @@ __le32 GetInodeFromBlock(int img, unsigned int block_size, off_t offset, unsigne
 			return -1;
 		}
 		int len = 0;
-		while (size > 0) {
+		while (len < size) {
 			struct ext2_dir_entry_2* dir = (struct ext2_dir_entry_2*)(buff + len);
 			if (dir->inode == 0)
 				break;
@@ -116,7 +116,6 @@ __le32 GetInodeFromBlock(int img, unsigned int block_size, off_t offset, unsigne
 				free(buff);
 				return dir->inode;
 			}
-			size -= dir->rec_len;
 			len += dir->rec_len;
 		}
 		*file_size = (*file_size > block_size) ? *file_size - block_size : 0;
@@ -249,7 +248,8 @@ int dump_file(int img, const char *path, int out) {
 		//printf("%s ", cur_name);
 	}
 	int file_inode = cur_parent_inode;
-	WriteFile(img, file_inode, out);
+	if (WriteFile(img, file_inode, out) != 0)
+		return -errno;
 	free(path_cpy);
 	return 0;
 }
