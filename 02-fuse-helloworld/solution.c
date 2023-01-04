a7f4e88b4a08ff437428c338f6965d950bbb9b11
gleb-mak <makarov.ge@phystech.edu>
2023-01-04 16:53:01 +0300

    add solution

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -1,13 +1,68 @@
-#include <solution.h>
+#include "solution.h"
 
+#include <errno.h>
 #include <fuse.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int hello_getattr(const char *path, struct stat *st, struct fuse_file_info *info) {
+	void(info);
+	memset(st, 0, sizeof(struct stat));
+	st->st_mode = S_IFREG | 0400;
+	st->st_nlink = 1;
+	st->st_uid = fuse_get_context()->uid;
+	st->st_gid = fuse_get_context()->gid;
+	st->st_atime = time(NULL);
+	st->st_mtime = time(NULL);
+	if (!strcmp(path, "/")) {
+		st->st_nlink = 2;
+		st->st_mode = S_IFDIR | 0755;
+	}
+	return 0;
+}
+
+int hello_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *info) {
+	(void)path; (void)buf; (void)size; (void)offset; (void)info;
+	return -EROFS;
+}
+
+int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *info, enum fuse_readdir_flags flags) {
+	void(offset); void(info); void(flags);
+	filler(buf, ".", NULL, 0);
+	filler(buf, "..", NULL, 0);
+	if (!strcmp(path, "/")) {
+		filler(buf, "hello", NULL, 0);
+	}
+	return 0;
+}
+
+int hello_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *info) {
+	void(info);
+	if (strcmp(path, "/"))
+		return -1;
+	char data[20] = { '\0' };
+	sprintf(data, "hello, %d\n", fuse_get_context()->pid);
+	int len = strlen(data);
+	if (offset >= len)
+		return 0;
+	len = (len - offset < size) ? len - offset : size;
+	memset(buf, data + offset, len);
+	return len;
+}
 
 static const struct fuse_operations hellofs_ops = {
-	/* implement me */
+	.getattr = hello_getattr,
+    .readdir = hello_readdir,
+    .read = hello_read,
+	.write = hello_write
 };
 
-int helloworld(const char *mntp)
-{
+int helloworld(const char *mntp) {
 	char *argv[] = {"exercise", "-f", (char *)mntp, NULL};
 	return fuse_main(3, argv, &hellofs_ops, NULL);
 }
